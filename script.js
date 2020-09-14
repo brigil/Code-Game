638d6d6d95107c42dff7ada1ead42950d8566a22
Brigitte G <urbrigette@gmail.com>
2020-09-14 00:43:11 -0400

    CSS/HTML/BUTTONS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var startBtn =document.getElementById("start")
+
+
 var questions = [
   {
     title: "Commonly used data types DO NOT include:",
@@ -9,5 +12,25 @@ var questions = [
     choices: ["1.Quotes", "2.Curly Braces", "3.Parentheses", "4.Square Brackets"],
     answer: "Parentheses"
   },
-{}
+ {
+  title: "Arrays in javaScript can be used to store __________.:",   
+  choices: ["1.Numbers and strings", "2.Other arrays", "3.Booleans", "4.All of the above"], 
+  answer: "All of the above"
+
+},
+{
+  title: "Sting values must be enclosed within:",
+  choices: ["1.Commas", "2.Curly braces", "3.Quotes", "4.Parentheses"],  
+  answer: "Quotes"  
+},
+
+{
+  title: "A very useful tool used during development and debugging for printing content to the debugger is:",   
+  choices: ["1.JavaScript", "2.Terminal/bash", "3.For loops", "4.Console.log"],
+  answer: "Console.log"
+},
 ];
+
+function beginGame(){
+  
+}
