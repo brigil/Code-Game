421471080d825f0ab94b8aa336454c34ae8b1a9e
Brigitte G <urbrigette@gmail.com>
2020-09-13 21:45:36 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["1.Strings\n", "2.Booleans", "3.Alerts", "4.Numbers"],
+    answer: "Alerts"
+  },
+  {
+    title: "The conditional in an if/else statement is enclosed in ________.:",
+    choices: ["1.Quotes", "2.Curly Braces", "3.Parentheses", "4.Square Brackets"],
+    answer: "Parentheses"
+  },
+{}
+];
